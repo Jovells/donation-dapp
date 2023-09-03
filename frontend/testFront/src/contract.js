842bbafb7051e0c26e5cf196da654f00564b219d
Jovells <Jovells.appiah@gmail.com>
2023-09-03 21:45:42 +0000

    Fixed Errors

--- frontend/testFront/src/contract.js
+++ frontend/testFront/src/contract.js
@@ -6,17 +6,13 @@ import { Network, Alchemy } from "alchemy-sdk";
 // dotenv.config();
 
 const settings = {
-    apiKey: import.meta.VITE_ALCHEMY_API_KEY || "",
+    apiKey: import.meta.env.VITE_ALCHEMY_API_KEY || "",
     network: Network.MATIC_MUMBAI,
 };
 const alchemy = new Alchemy(settings);
 
 
 const CONTRACT_ADDRESS = "0x4725F47b49E508856Afd6F0badDCb28d8f4427F6";
-// const p2 = new ethers.JsonRpcProvider("https://polygon-mumbai.g.alchemy.com/v2/fjfWrvNtk0DZ2dlAlWPk1I7FhdZemfrj");
-const provider = new ethers.providers.WebSocketProvider(`wss://polygon-mumbai.g.alchemy.com/v2/${import.meta.VITE_ALCHEMY_API_KEY || "" }`)
+const provider = new ethers.providers.WebSocketProvider(`wss://polygon-mumbai.g.alchemy.com/v2/${import.meta.env.VITE_ALCHEMY_API_KEY || "" }`)
 
-// new alchemy.ws.on(eventName, listener)
 export const donation = new ethers.Contract(CONTRACT_ADDRESS, ABI, provider )
-
-// provider.websocket.onopen(()=>console.log("connected"))
