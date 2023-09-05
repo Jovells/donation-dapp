cfdaf26ef23a9fdb170e45000a7219ec416af611
Jovells <Jovells.appiah@gmail.com>
2023-09-05 19:35:47 +0000

    fixed events noy firing

--- frontend/testFront/src/App.jsx
+++ frontend/testFront/src/App.jsx
@@ -1,50 +1,60 @@
-import { useContext, useEffect, useState } from 'react'
-import './App.css'
+import { useContext, useEffect, useState } from "react";
+import "./App.css";
 import { useMetaMask } from "metamask-react";
-import Donate from './pages/Donate';
+import Donate from "./pages/Donate";
 import { ethers } from "ethers";
-import DonationHistory from './pages/DonationHistory';
-import ChangeBeneficiary from './pages/ChangeBeneficiary';
-import PauseOperations from './pages/PauseOperations';
-import { EthersContext } from './utils/EtherContext';
-import WithdrawFunds from './pages/WithdrawFunds';
-import { donationContract } from './contract';
+import DonationHistory from "./pages/DonationHistory";
+import ChangeBeneficiary from "./pages/ChangeBeneficiary";
+import PauseOperations from "./pages/PauseOperations";
+import { EthersContext } from "./utils/EtherContext";
+import WithdrawFunds from "./pages/WithdrawFunds";
+import { donationContract } from "./contract";
 
 function App() {
   const [beneficiary, setBeneficiary] = useState("");
   const [totalDonations, setTotalDonations] = useState(0);
-  const { donation, status, connect, account, chainId, ethereum } = useContext(EthersContext)
+  const { donation, status, connect, account, chainId, ethereum } =
+    useContext(EthersContext);
 
   const { switchChain } = useMetaMask();
 
   useEffect(() => {
-    donationContract.on("FundsWithdrawn", (toBeneficiary, amount, timestamp) => {
-      console.log('FundsWithdrawn: ', toBeneficiary, 'amount: ', ethers.utils.formatEther(amount), 'timestamp: ', timestamp);
-      (toBeneficiary === beneficiary) && donation.getAmountReceived(beneficiary).then((donationsAmount) => {
-        setTotalDonations(ethers.formatEther(donationsAmount));
-      })
-    })
+    donationContract.on(
+      "FundsWithdrawn",
+      (toBeneficiary, amount, timestamp) => {
+        console.log(
+          "FundsWithdrawn: ",
+          toBeneficiary,
+          "amount: ",
+          ethers.utils.formatEther(amount),
+          "timestamp: ",
+          timestamp
+        );
+        toBeneficiary === beneficiary &&
+          donation.getAmountReceived(beneficiary).then((donationsAmount) => {
+            setTotalDonations(ethers.formatEther(donationsAmount));
+          });
+      }
+    );
     donationContract.on("BeneficiaryChanged", (newBeneficiary) => {
-      console.log('newBeneficiary: ', newBeneficiary);
+      console.log("newBeneficiary: ", newBeneficiary);
       console.log(newBeneficiary !== beneficiary);
-      setBeneficiary(oldBeneficiary => {
+      setBeneficiary((oldBeneficiary) => {
         if (newBeneficiary !== oldBeneficiary) {
           donation.getAmountReceived(newBeneficiary).then((donationsAmount) => {
-              setTotalDonations(ethers.formatEther(donationsAmount));
-            })
+            setTotalDonations(ethers.formatEther(donationsAmount));
+          });
           return newBeneficiary;
         }
         return oldBeneficiary;
       });
-    })
+    });
 
-    return () => { 
+    return () => {
       donationContract?.removeAllListeners("FundsWithdrawn");
       donationContract?.removeAllListeners("BeneficiaryChanged");
-    }
-
-  }, [donationContract])
-
+    };
+  }, [donation]);
 
   useEffect(() => {
     async function getCurrentBeneficiary() {
@@ -54,46 +64,49 @@ function App() {
         // console.log({signer, donation});
         // Make Function Call
         try {
-          const currentBeneficiary = await donation.currentBeneficiary()
+          const currentBeneficiary = await donation.currentBeneficiary();
           setBeneficiary(currentBeneficiary);
-          const donationsAmount = await donation.getAmountReceived(currentBeneficiary);
+          const donationsAmount = await donation.getAmountReceived(
+            currentBeneficiary
+          );
           setTotalDonations(ethers.formatEther(donationsAmount));
-
-
         } catch (error) {
-          console.log(error)
+          console.log(error);
         }
       }
     }
 
     getCurrentBeneficiary();
 
-    return () => {
-      donationContract?.removeAllListeners();
-    }
-  }, [ethereum, donation, beneficiary])
+    return () => {};
+  }, [ethereum, donation, beneficiary]);
 
+  if (status === "initializing")
+    return <div>Synchronisation with MetaMask ongoing...</div>;
 
-  if (status === "initializing") return <div>Synchronisation with MetaMask ongoing...</div>
+  if (status === "unavailable") return <div>MetaMask not available </div>;
 
-  if (status === "unavailable") return <div>MetaMask not available </div>
+  if (status === "notConnected")
+    return <button onClick={connect}>Connect to MetaMask</button>;
 
-  if (status === "notConnected") return <button onClick={connect}>Connect to MetaMask</button>
+  if (status === "connecting") return <div>Connecting...</div>;
 
-  if (status === "connecting") return <div>Connecting...</div>
-
-  if (status === "connected" && chainId != "0x13881") return (
-    <div>
-      <h1>You're on the wrong network</h1>
-      <button onClick={() => switchChain("0x13881")}>Switch to MATIC Mumbai</button>
-    </div>
-  )
-
-  if (status === "connected" && chainId === "0x13881")
-
+  if (status === "connected" && chainId != "0x13881")
     return (
       <div>
-        <div>Connected account {account} on chain ID {chainId}</div>
+        <h1>You're on the wrong network</h1>
+        <button onClick={() => switchChain("0x13881")}>
+          Switch to MATIC Mumbai
+        </button>
+      </div>
+    );
+
+  if (status === "connected" && chainId === "0x13881")
+    return (
+      <div>
+        <div>
+          Connected account {account} on chain ID {chainId}
+        </div>
         <div>Current Beneficiary: {beneficiary}</div>
         <div>Total Donations: {totalDonations} MATIC</div>
         {/* Import Add EC Page */}
@@ -115,12 +128,13 @@ function App() {
         <hr />
         {/* <h3>WithdrawalHistory</h3> */}
         {/* <WithdrawalHistory /> */}
-        <br /><br /><br />
+        <br />
+        <br />
+        <br />
       </div>
-    )
-
+    );
 
   return null;
 }
 
-export default App
+export default App;
